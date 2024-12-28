5d78ed48f648edef6572b43c29dec84eaf7e582d
李嘉图·M·路 <146103794+Ricardo-M-Zheng@users.noreply.github.com>
2024-12-29 01:24:11 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,11 +19,11 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
+  trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'SisyphusZheng', // Usually your GitHub org/user name.
-  projectName: 'aircomponents', // Usually your repo name.
+  projectName: 'Aircomponents-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
