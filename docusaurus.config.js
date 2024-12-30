f3cf3ef7e97a0c3567393da4e3ddc22ca2dc2888
李嘉图·M·路 <146103794+Ricardo-M-Zheng@users.noreply.github.com>
2024-12-30 18:54:37 +0800

    fix:url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://SisyphusZheng.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: ' /AirCom-Docs/',
+  baseUrl: '/AirCom-Docs/',
   trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
